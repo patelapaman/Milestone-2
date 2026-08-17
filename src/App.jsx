1b79b628e32b905a980a68a334a978fd2dff064b
Rivan Dsilva <rivandsilva@gmail.com>
2026-08-17 16:22:57 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { NavLink,Routes,Route } from "react-router-dom";
-import { Shield,Radio,ScanSearch } from "lucide-react";
-import Dashboard from "./pages/Dashboard";
-import EventDetails from "./pages/EventDetails";
-import LivePrediction from "./pages/LivePrediction";
-export default function App(){
- return <><nav className="side-nav"><div className="nav-logo"><Shield/></div><NavLink to="/" end title="Dashboard"><Radio/></NavLink><NavLink to="/live" title="Live Prediction"><ScanSearch/></NavLink></nav><Routes><Route path="/" element={<Dashboard/>}/><Route path="/events/:id" element={<EventDetails/>}/><Route path="/live" element={<LivePrediction/>}/></Routes></>
-}
